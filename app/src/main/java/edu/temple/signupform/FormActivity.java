5f660fcd9f534299423f6189009fcf9143e33101
Tarek <tug35668@temple.edu>
2019-09-18 12:45:59 -0400

    Added logic to get user input and validate information

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,9 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.submitButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = ((TextView) findViewById(R.id.nameText)).getText().toString();
+                String email = ((TextView) findViewById(R.id.emailText)).getText().toString();
+                String pass = ((TextView) findViewById(R.id.passText)).getText().toString();
+                String passConfirm = ((TextView) findViewById(R.id.confirmPass)).getText().toString();
+                if(name.length() == 0 || email.length() == 0 || pass.length() == 0 || passConfirm.length() == 0){
+                    Toast.makeText(FormActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(!pass.equals(passConfirm)){
+                    Toast.makeText(FormActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Toast.makeText(FormActivity.this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
